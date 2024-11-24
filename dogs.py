63114356355a6f3438a8f96e4caf46c2e13d13b8
irinaumova01 <irinaumova01@gmail.com>
2024-11-24 16:33:32 +0500

    создана функция загрузки ссылки на изображение

--- dogs.py
+++ dogs.py
@@ -4,6 +4,19 @@ from PIL import Image, ImageTk
 from io import BytesIO
 from tkinter import messagebox as mb
 
+from bottle import response
+
+
+def get_dog_image():
+    try:
+        response=requests.get("https://dog.ceo/api/breeds/image/random")
+        response.raise_for_status()
+        data=response.json()
+        return data("message")
+    except Exception as e:
+        mb.showerror("Ошибка!", f"Произошла ошибка при загрузке изображения: {e}")
+        return None
+
 
 def show_image():
     image_url=get_dog_image()
@@ -14,6 +27,7 @@ def show_image():
             img_data=BytesIO(response.content)
             img=Image.open(img_data)
             img.thumbnail((300, 300))
+            img=ImageTk.PhotoImage(img)
             label.config(image=img)
             label.image=img
         except Exception as e:
